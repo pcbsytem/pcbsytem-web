5ace5d59039ff3321aa9da2a7333959a8dd5ab09
Patrick <pkcs@gft.com>
2023-09-11 10:30:52 -0300

    improve css style with vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import styles from './page.module.css'
-import { Experience, Hero, Skills, Stars } from '@/component'
+import { Experience, Hero, Skills, Stars, AboutMe } from '@/component'
 
 export default function Home() {
   return (
@@ -7,6 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Hero />
         <div className={styles.container}>
+          <AboutMe />
           <Skills />
           <Experience />
         </div>
---
+++ src/component/AboutMe/index.tsx
@@ -0,0 +1,37 @@
+import { ContentContainer } from '../ContentContainer'
+import './index.css'
+
+export function AboutMe() {
+  return (
+    <ContentContainer title="About Me">
+      <div className="about-me">
+        <h4>
+          Experienced front-end developer with skills in web and mobile
+          technologies
+        </h4>
+        <br />
+        <p>
+          Hi, I’m a front-end developer with 7 years of experience in web and
+          mobile technologies. I have expertise in NextJS, ReactJS, React
+          Native, Angular, and Typescript. I have worked on a lot of kinds of
+          projects, from small websites to large-scale web applications and
+          mobile apps. I have mentored junior and mid-level front-end developers
+          and helped them improve their coding skills and best practices. I have
+          also worked on a project with GFT Costa Rica, a leading IT company,
+          where I built a web app that manages industry assets. I have developed
+          web and mobile apps for various platforms, such as iOS and Android,
+          using React Native and native technologies. I have a keen eye for
+          design and usability and I can create beautiful and user-friendly
+          interfaces that work well on different devices and resolutions. I can
+          work under pressure and deliver high-quality results within tight
+          deadlines. I am proactive and always eager to learn new things and
+          take on new challenges. I work fast and efficiently, without
+          compromising quality or functionality. I love web and mobile
+          development and I enjoy creating amazing user experiences that solve
+          real problems. I am looking for long-term or short-term projects where
+          I can use my skills and knowledge to help you achieve your goals.
+        </p>
+      </div>
+    </ContentContainer>
+  )
+}
